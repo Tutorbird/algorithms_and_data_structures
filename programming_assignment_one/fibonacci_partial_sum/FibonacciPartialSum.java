af7bbaa5fd5b26bf0e77def26bf3cca406b2038a
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-11 17:58:35 -0800

    Remove debugging statements. Refactor code to get correct answer.

--- programming_assignment_one/fibonacci_partial_sum/FibonacciPartialSum.java
+++ programming_assignment_one/fibonacci_partial_sum/FibonacciPartialSum.java
@@ -1,49 +1,71 @@
 import java.util.*;
 
 public class FibonacciPartialSum {
-    private static long getFibonacciPartialSumNaive(long from, long to) {
+    private static long getFibonacciPartialSumNaive(long from, long n) {
 
-        if (to <= 1)
-            return to;
+        if (n <= 1)
+          return n;
+
+        /*
+         we get the remainder, or count, by using the formula given
+         in the assignment. n = (x * period) + remainder. As such...
+         remainder = n % period
+        
+         We add 2 to n since S(n) = F(n + 2) - 1
+         We initialize the m to 1000 in order to do subtraction from 
+         the upper bound to the lower bound. Likewise, we return a mod 10
+         value in order to get only the last digit.
+        */
+
+        long m = 1000; 
+        long period = getPeriodLength(m);
+        long upper = ( (n + 2) % period);
+        long lower = ( (from + 1) % period);
+        long sum_lower = getFibonacciModM(lower, m) - 1;
+        long sum_upper = getFibonacciModM(upper, m) - 1;
+        long sum = sum_upper - sum_lower;
+        if (sum == -1)
+            sum = 9;
+
+        return sum % 10;
+        
+    }
+
+    private static long getFibonacciModM(long n, long m) {
+        if (n <= 1)
+            return n;
 
         long previous = 0;
         long current  = 1;
-        long min = 0;
 
-        for (long i = 0; i < to + 1; ++i) {
+        for (int i = 0; i < n - 1; ++i) {
             long tmp_previous = previous;
             previous = current;
-            current = tmp_previous + current;
-            if (i == from - 1)
-                min = current - 1;
+            current = (tmp_previous % m) + (current % m);
         }
 
-        System.out.println("Min: " + min + ", Current: " + (current - 1));
+        return current % m;
+    }
 
-        return (current - min - 1) % 10;
+    private static long getPeriodLength(long m) {
 
-        // if (to <= 1)
-        //     return to;
+      long previous_two = 0;
+      long previous_one = 1;
+      long current = 1;
+      long i = 0;
+      /*
+        This is based on the principal given in the assignment that each
+        recurrence starts with a 0 and 1
+      */
+      do {
+          current = (previous_one + previous_two) % m;
+          previous_two = previous_one;
+          previous_one = current;
+          ++i;
+      } while ( !(previous_two == 0 && previous_one == 1) );
+    
+      return i;
 
-        // long previous = 0;
-        // long current  = 1;
-
-        // for (long i = 0; i < from - 1; ++i) {
-        //     long tmp_previous = previous;
-        //     previous = current;
-        //     current = tmp_previous + current;
-        // }
-
-        // long sum = current;
-
-        // for (long i = 0; i < to - from; ++i) {
-        //     long tmp_previous = previous;
-        //     previous = current;
-        //     current = tmp_previous + current;
-        //     sum += current;
-        // }
-
-        // return sum % 10;
     }
     
     public static void main(String[] args) {
