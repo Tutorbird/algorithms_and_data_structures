8c045ca7c2c022f7ed8027ba69b6e38aded07d33
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-14 01:46:15 -0800

    Naive algorithm implemented with bad data types.

--- programming_assignment_two/fractional_knapsack/FractionalKnapsack.java
+++ programming_assignment_two/fractional_knapsack/FractionalKnapsack.java
@@ -4,10 +4,34 @@ import java.text.DecimalFormat;
 public class FractionalKnapsack {
     private static double getOptimalValue(int capacity, int[] values, int[] weights) {
         double value = 0;
-        //write your code here
+          
+        for (int i = 0; i < values.length; i++) {
+          if (capacity == 0)
+            return value;
+
+          double temp = 0;
+          double quotient = 0;
+          int idx = 0;
+
+          for (int j = 0; j < values.length; j++) {
+            temp = (double) values[j] / weights[j];
+            if (  temp > quotient ) {
+              quotient = temp;
+              idx = j;
+            }
+          }
+
+          int min_value = Math.min(weights[idx], capacity);
+          value += min_value * quotient;
+          capacity -= min_value;
+          if (min_value == weights[idx]) {
+            values[idx] = 0;
+            weights[idx] = 1;
+          }
+        }
 
         return value;
-    }
+      }
 
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
