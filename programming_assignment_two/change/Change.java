98e6fe7ca6871717143cca0e24e6b108cdd42364
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-13 21:19:03 -0800

    Delete non-Java and non-Ruby files. Implement a coin changer for values 1, 5, and 10.

--- programming_assignment_two/change/Change.java
+++ programming_assignment_two/change/Change.java
@@ -2,8 +2,22 @@ import java.util.Scanner;
 
 public class Change {
     private static int getChange(int m) {
-        //write your code here
-        return m;
+      /*
+        For the sake of this assignment,
+        we simply need to return the number of
+        coins dispensed not their actual values.
+        
+        Restraints: 1 <= m <= 10^3
+        Coin values to check are 1, 5, and 10
+      */
+
+      int coins = Math.round(m/10);
+      m %= 10;
+      coins += Math.round(m/5);
+      m %= 5;
+      coins += Math.round(m/1);
+
+      return coins;
     }
 
     public static void main(String[] args) {
