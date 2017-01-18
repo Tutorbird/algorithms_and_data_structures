70ed99999e6632cd14598bd796c3448da366c00e
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-18 03:43:13 -0800

    Yes, basic tests passed.

--- programming_assignment_two/different_summands/DifferentSummands.java
+++ programming_assignment_two/different_summands/DifferentSummands.java
@@ -2,8 +2,25 @@ import java.util.*;
 
 public class DifferentSummands {
     private static List<Integer> optimalSummands(int n) {
+        /*
+         The logic below is based on:
+          (n - (i + 1)) > (i + 1)
+          Needless reduction to two math ops 
+         */
         List<Integer> summands = new ArrayList<Integer>();
-        //write your code here
+        if (n < 3) {
+            summands.add(n);
+            return summands;
+        } else {
+            int i = 0;
+            do {
+                i++;
+                summands.add(i);
+                n -= i;
+            } while (n - i > i + 2);
+            summands.add(n);
+        }
+
         return summands;
     }
     
--- programming_assignment_two/largest_number/LargestNumber.java
+++ programming_assignment_two/largest_number/LargestNumber.java
@@ -1,13 +1,34 @@
 import java.util.*;
 
 public class LargestNumber {
-    private static String largestNumber(String[] a) {
-        //write your code here
-        String result = "";
-        for (int i = 0; i < a.length; i++) {
-            result += a[i];
+    private static boolean isGreaterThanOrEqual(String a, String b) {
+        if (a.charAt(0) >= b.charAt(0) && a.length() <= b.length()) {
+          return true;
         }
-        return result;
+
+        return false;
+      }
+
+    private static String largestNumber(String[] a) {
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (i != j && isGreaterThanOrEqual(a[i], a[j]))  {
+                    String temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+
+            }
+        }
+
+        for(String s : a) {
+            builder.append(s);
+        }
+
+        return builder.toString();
     }
 
     public static void main(String[] args) {
