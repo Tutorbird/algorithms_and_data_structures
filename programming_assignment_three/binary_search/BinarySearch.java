d82f4434a662a739912d42f828087931d7d8fd8a
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-19 02:05:32 -0800

    Implement miracle midpoint case.

--- programming_assignment_three/binary_search/BinarySearch.java
+++ programming_assignment_three/binary_search/BinarySearch.java
@@ -4,8 +4,14 @@ import java.util.*;
 public class BinarySearch {
 
     static int binarySearch(int[] a, int x) {
+        /*
+         If we get one or the initial midpoint is correct,
+         return that index. Else we decide to look left
+         or right.
+         */
+
         int m = (a.length) / 2;
-        
+
         if (a.length == 1 && a[0] == x) {
             return 0;
         } else if (a[m] == x) {
@@ -16,20 +22,24 @@ public class BinarySearch {
     }
 
     public static void main(String[] args) {
-        FastScanner scanner = new FastScanner(System.in);
-        int n = scanner.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scanner.nextInt();
-        }
-        int m = scanner.nextInt();
-        int[] b = new int[m];
-        for (int i = 0; i < m; i++) {
-          b[i] = scanner.nextInt();
-        }
-        for (int i = 0; i < m; i++) {
+        int[] data = {0,1,2,3};
 
-        }
+        System.out.println(binarySearch(data, 1));
+
+        // FastScanner scanner = new FastScanner(System.in);
+        // int n = scanner.nextInt();
+        // int[] a = new int[n];
+        // for (int i = 0; i < n; i++) {
+        //     a[i] = scanner.nextInt();
+        // }
+        // int m = scanner.nextInt();
+        // int[] b = new int[m];
+        // for (int i = 0; i < m; i++) {
+        //   b[i] = scanner.nextInt();
+        // }
+        // for (int i = 0; i < m; i++) {
+
+        // }
     }
     static class FastScanner {
         BufferedReader br;
