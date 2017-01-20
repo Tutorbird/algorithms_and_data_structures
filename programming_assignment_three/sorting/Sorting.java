68fa29c7dac50d83688f30fd36901ca5d2e6b21a
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-20 08:54:20 -0800

    Implement quicksort. I was overthinking the simple yet again.

--- programming_assignment_three/sorting/Sorting.java
+++ programming_assignment_three/sorting/Sorting.java
@@ -10,48 +10,26 @@ public class Sorting {
          we check if it's 
         */
         int x = a[l];
-        int count = l, lower = 0;
         int[] m = new int[2];
+        int count = l;
 
         for (int i = l + 1; i <= r; i++) {
             if (a[i] == x) {
                 count++;
-                if (count != i) {
-                    int t = a[count];
-                    a[count] = a[i];
-                    a[i] = a[count + lower];
-                    a[count + lower] = t;
-                }              
-            } else if (a[i] < x) {
-                lower++;
-                if ((count + lower) != i) {
-                    int t = a[count + lower];
-                    a[count + lower] = a[i];
-                    a[i] = t;
-                }
-            }
-        }
-
-        m[0] = count;
-        m[1] = lower;
-      return m;
-    }
-
-    private static int partition2(int[] a, int l, int r) {
-        int x = a[l];
-        int j = l;
-        for (int i = l + 1; i <= r; i++) {
-            if (a[i] <= x) {
-                j++;
                 int t = a[i];
-                a[i] = a[j];
-                a[j] = t;
+                a[i] = a[count];
+                a[count] = t;
+            } else if (a[i] < x) {
+                a[l] = a[i];
+                a[i] = a[count + 1];
+                a[count + 1] = x;
+                l++;
             }
         }
-        int t = a[l];
-        a[l] = a[j];
-        a[j] = t;
-        return j;
+
+        m[0] = l;
+        m[1] = count;
+      return m;
     }
 
     private static void randomizedQuickSort(int[] a, int l, int r) {
@@ -64,8 +42,8 @@ public class Sorting {
         a[k] = t;
         //use partition3
         int[] m = partition3(a, l, r);
-        randomizedQuickSort(a, m[0] + 1, m[0] + m[1]);
-        randomizedQuickSort(a, m[0] + m[1] + 1, r);
+        randomizedQuickSort(a, l, m[0] - 1);
+        randomizedQuickSort(a, m[1] + 1, r);
     }
 
     public static void main(String[] args) {
