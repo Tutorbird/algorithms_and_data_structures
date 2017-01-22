db4b7747b11a10090fa30005cda111e14bade8a2
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-22 04:41:18 -0800

    I disliked this implementation very, very much.

--- programming_assignment_four/knapsack/Knapsack.java
+++ programming_assignment_four/knapsack/Knapsack.java
@@ -1,25 +1,29 @@
 import java.util.*;
 
 public class Knapsack {
-    static int optimalWeight(int W, int[] w) {
-        int max_result = 0;
+    private static int optimalWeight(int W, int[] w) {
+        int K[][] = new int[w.length + 1][W + 1];
         
-        for (int i = w.length - 1; i >= 0; i--) {
-          int result = 0;  
-          for (int j = i; j >= 0; j--) {
-            if (result + w[j] <= W) {
-              result += w[j];
+        // Build table K[][] in bottom up manner
+        for (int i = 1; i <= w.length; i++) {
+          for (int j = 1; j <= W; j++) {
+            int value = K[i - 1][j];
+            
+            if (w[i - 1] <= j) {
+                int val = (K[i - 1][j - w[i - 1]]) + w[i - 1];
+                if (value < val) {
+                  value = val;
+                  K[i][j] = value;
+                } else {
+                    K[i][j] = value;
+                }
+            } else {
+                K[i][j] = value;
             }
           }
-          
-          if (i == w.length - 1) {
-            max_result = result;
-          } else if (result > max_result) {
-            max_result = result;
-          }
-          
         }
-        return max_result;
+        
+        return K[w.length][W];
     }
 
     public static void main(String[] args) {
