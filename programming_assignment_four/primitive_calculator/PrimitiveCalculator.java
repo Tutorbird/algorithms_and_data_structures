15efe912cb77096f1d196f8463c3668351254ad7
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-22 05:42:15 -0800

    Implement primitive calculator that returns the min number of operations to get to n by either multiplying by 2 or 3 or adding 1.

--- programming_assignment_four/primitive_calculator/PrimitiveCalculator.java
+++ programming_assignment_four/primitive_calculator/PrimitiveCalculator.java
@@ -1,18 +1,28 @@
 import java.util.*;
+import java.lang.Math;
 
 public class PrimitiveCalculator {
     private static List<Integer> optimal_sequence(int n) {
         List<Integer> sequence = new ArrayList<Integer>();
-        while (n >= 1) {
-            sequence.add(n);
-            if (n % 3 == 0) {
-                n /= 3;
-            } else if (n % 2 == 0) {
-                n /= 2;
-            } else {
-                n -= 1;
-            }
+        
+        int[] arr = new int[n + 1];
+        
+        for (int i = 1; i < arr.length; i++) {
+          arr[i] = arr[i - 1] + 1;
+          if (i % 2 == 0) arr[i] = Math.min(1 + arr[i / 2], arr[i]);
+          if (i % 3 == 0) arr[i] = Math.min(1 + arr[i / 3], arr[i]);
         }
+    
+        for (int i = n; i > 1; ) {
+          sequence.add(i);
+          if (arr[i - 1] == arr[i] - 1)
+              i = i - 1;
+          else if (i % 2 == 0 && (arr[i / 2] == arr[i] - 1))
+              i = i / 2;
+          else if (i % 3 == 0 && (arr[i / 3] == arr[i] - 1))
+              i = i / 3;
+        }
+        sequence.add(1);
         Collections.reverse(sequence);
         return sequence;
     }
