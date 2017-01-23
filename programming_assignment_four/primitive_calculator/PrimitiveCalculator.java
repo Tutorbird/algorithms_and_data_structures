f19bf9110d89fe2bb2e9f95c8637abc9e5ddd418
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-22 21:44:01 -0800

    Implement a primitive calculator to determine the minimum number of steps required to get the nth values.

--- programming_assignment_four/primitive_calculator/PrimitiveCalculator.java
+++ programming_assignment_four/primitive_calculator/PrimitiveCalculator.java
@@ -4,23 +4,38 @@ import java.lang.Math;
 public class PrimitiveCalculator {
     private static List<Integer> optimal_sequence(int n) {
         List<Integer> sequence = new ArrayList<Integer>();
-        
+        /*
+         Initiate the size of the array to the worst-case of adding one
+         over and over. 
+         We do the safest action of adding first. Then we check whether
+         it's divisible by 2 or 3. If it is, we check the last place where
+         this growing number was last divisible by that number. We add one
+         to that operation list. We check whether the least mutable action
+         generated the lowest number of operations or if the last index
+         where the number was divisible by the number being checked
+        (+ 1 for the current operation) and set it to the new ith value.
+         */
         int[] arr = new int[n + 1];
         
-        for (int i = 1; i < arr.length; i++) {
-          arr[i] = arr[i - 1] + 1;
-          if (i % 2 == 0) arr[i] = Math.min(1 + arr[i / 2], arr[i]);
-          if (i % 3 == 0) arr[i] = Math.min(1 + arr[i / 3], arr[i]);
+        for (int i = 1; i < n + 1; i++) {
+            arr[i] = arr[i - 1] + 1;
+            if (i % 2 == 0) {
+                arr[i] = Math.min(arr[i / 2] + 1, arr[i]);
+            }
+            if (i % 3 == 0) {
+                arr[i] = Math.min(arr[i / 3] + 1, arr[i]);
+            }   
         }
     
         for (int i = n; i > 1; ) {
-          sequence.add(i);
-          if (arr[i - 1] == arr[i] - 1)
-              i = i - 1;
-          else if (i % 2 == 0 && (arr[i / 2] == arr[i] - 1))
-              i = i / 2;
-          else if (i % 3 == 0 && (arr[i / 3] == arr[i] - 1))
-              i = i / 3;
+            sequence.add(i);
+            if (arr[i - 1] == arr[i] - 1) {
+                i -= 1;
+            } else if (i % 2 == 0 && (arr[i / 2] == arr[i] - 1)) {
+                i /= 2;
+            } else if (i % 3 == 0 && (arr[i / 3] == arr[i] - 1)) {
+                i /= 3;
+            }
         }
         sequence.add(1);
         Collections.reverse(sequence);
