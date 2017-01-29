4db51140ad3af6233d9f7acd433a17be0cb51e29
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-29 04:39:19 -0800

    Implement simple cases where there is not enough brackets to solve after iterating once or if there isn't enough members in the stack to check any remaining.

--- course_two_data_structures/programming_assignment_one/check_brackets_in_code/check_brackets.java
+++ course_two_data_structures/programming_assignment_one/check_brackets_in_code/check_brackets.java
@@ -3,6 +3,40 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.Stack;
 
+class check_brackets {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader input_stream = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input_stream);
+        String text = reader.readLine();
+
+        Stack<Bracket> opening_brackets_stack = new Stack<Bracket>();
+        for (int position = 0; position < text.length(); ++position) {
+            char next = text.charAt(position);
+
+            if (next == '(' || next == '[' || next == '{') {
+                opening_brackets_stack.push(new Bracket(next, position));
+            } else if (next == ')' || next == ']' || next == '}') {
+                if (opening_brackets_stack.empty()) {
+                    System.out.println(position + 1);
+                } else {
+                    System.out.println("Entering here");
+                    Bracket temp = opening_brackets_stack.pop();
+                    if (!temp.Match(next)) {
+                        System.out.println(position);
+                    }
+                }
+
+            }
+        }
+
+        if (!opening_brackets_stack.empty()) {
+            System.out.println(opening_brackets_stack.pop().position + 1);
+        } else {
+            System.out.println("Success");
+        }
+    }
+}
+
 class Bracket {
     Bracket(char type, int position) {
         this.type = type;
@@ -23,25 +57,3 @@ class Bracket {
     int position;
 }
 
-class check_brackets {
-    public static void main(String[] args) throws IOException {
-        InputStreamReader input_stream = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(input_stream);
-        String text = reader.readLine();
-
-        Stack<Bracket> opening_brackets_stack = new Stack<Bracket>();
-        for (int position = 0; position < text.length(); ++position) {
-            char next = text.charAt(position);
-
-            if (next == '(' || next == '[' || next == '{') {
-                // Process opening bracket, write your code here
-            }
-
-            if (next == ')' || next == ']' || next == '}') {
-                // Process closing bracket, write your code here
-            }
-        }
-
-        // Printing answer, write your code here
-    }
-}
